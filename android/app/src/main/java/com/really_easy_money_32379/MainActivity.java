c7ea2b9bf658df3e1b3d81a50e19f6c04e180158
Crowdbotics <team@crowdbotics.com>
2021-12-14 06:02:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/really_easy_money_32379/MainActivity.java
@@ -0,0 +1,15 @@
+package com.really_easy_money_32379;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "really_easy_money_32379";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "really_easy_money_32379",
+  url: "https://really_easy_money_32379.botics.co",
+  api: "https://really_easy_money_32379.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
